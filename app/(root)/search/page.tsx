f6e6377f14dc3d504cfaae35a21deb73d095b924
Andrés Esteban Rodríguez Jiménez <andrudeluxe@gmail.com>
2023-08-15 17:22:27 -0500

    add search bar and paginate

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,14 +1,13 @@
 import { redirect } from 'next/navigation'
 import { currentUser } from '@clerk/nextjs'
 
-// import UserCard from '@/components/cards/UserCard'
-// import Searchbar from '@/components/shared/Searchbar'
-// import Pagination from '@/components/shared/Pagination'
+import UserCard from '@/components/cards/UserCard'
+import Searchbar from '@/components/shared/Searchbar'
+import Pagination from '@/components/shared/Pagination'
 
 import { fetchUser, fetchUsers } from '@/lib/actions/user.actions'
-import UserCard from '@/components/cards/UserCard'
 
-export default async function Page({
+async function Page({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined }
@@ -30,9 +29,11 @@ export default async function Page({
     <section>
       <h1 className="head-text mb-10">Search</h1>
 
+      <Searchbar routeType="search" />
+
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
-          <p className="no-result">No users</p>
+          <p className="no-result">No Result</p>
         ) : (
           <>
             {result.users.map((person) => (
@@ -48,6 +49,14 @@ export default async function Page({
           </>
         )}
       </div>
+
+      <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </section>
   )
 }
+
+export default Page
